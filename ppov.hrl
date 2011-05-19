33c1ab5c10b3b18c07273d9cb3aa6704910b60b8
Olivier Girondel <olivier@biniou.info>
2011-05-19 21:24:37 +0200

    Until we have a supervisor and use yaws_api:embedded_start_conf

--- ppov.hrl
+++ ppov.hrl
@@ -7,8 +7,8 @@
 
 -ifdef(LOCALHOST).
 -define(CONTROL_HOST, "control.ppov.localhost").
--define(DATA_HOST,    "data.ppov.localhost").
+%% -define(DATA_HOST,    "data.ppov.localhost").
 -else.
--define(CONTROL_HOST, "control.ppov.home.biniou.net").
--define(DATA_HOST,    "data.ppov.home.biniou.net").
+-define(CONTROL_HOST, "home.biniou.net").
+%% -define(DATA_HOST,    "data.ppov.home.biniou.net").
 -endif.
